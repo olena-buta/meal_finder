50cc807bd0df3c748a5b1d4b5a3008eafdc79211
Olena Buta <olena.buta@gmail.com>
2020-07-15 00:07:09 +0300

    added showing single meal page

--- js/script.js
+++ js/script.js
@@ -16,7 +16,6 @@ function searchMeal(e) {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
       .then(response => response.json())
       .then(data => {
-        console.log(data.meals);
         resultHeading.innerHTML = `<h2>Search result for <strong>${term}</strong>:</h2>`;
         search.value = '';
 
@@ -43,4 +42,52 @@ function showNotification() {
   setTimeout(() => notification.classList.remove('show'), 2000);
 }
 
+function showSingleMealEl(id) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+    .then(response => response.json())
+    .then(data => {
+      const meal = data.meals[0];
+
+      addMealToDOM(meal);
+    });
+}
+
+function addMealToDOM(meal) {
+  const ingredients = [];
+
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(`${meal[`strMeasure${i}`]} - ${meal[`strIngredient${i}`]}`)
+    } else {
+      break;
+    }
+  }
+
+  singleMealEl.innerHTML = `
+    <div class="single-meal">
+      <h1>${meal.strMeal}</h1>
+      <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+      <div class="single-meal-info">
+        ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+        ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
+      </div>
+      <div class="main">
+        <h2>Ingredients</h2>
+        <ul>
+          ${ingredients.map(ingredient => `
+            <li>${ingredient}</li>
+          `).join('')}
+        </ul>
+        <p>${meal.strInstructions}</p>
+      </div>
+    </div>
+  `;
+}
+
 submit.addEventListener('submit', searchMeal);
+
+mealEl.addEventListener('click', e => {
+  const mealItem = e.target;
+  const mealID = mealItem.getAttribute('data-mealid');
+  showSingleMealEl(mealID);
+});
